fe09f74b1b186f858dc2eb5eefcd0203faffae2c
NischayDesigns4it <nischayv@designi4t.com>
2023-12-14 18:12:16 +0530

    add some new api's

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import  transaction, getBuyDate, VehicleList
+from .views import transaction, getBuyDate, VehicleList
 from . import views
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path("create/", views.enter_vehicle_data, name="create"),
     path('transaction-log/<str:date>/', getBuyDate.as_view(), name='get_buy_date'),
     path("vehicle-list/", VehicleList.as_view(), name="VehicleList-log"),
-    path("transactions/", transaction.as_view(), name="transactions-log"),
+    path("transaction/", transaction.as_view(), name="transaction"),
 
     # path("odometer/", odometer.as_view(), name="odometer"),
 
@@ -21,6 +21,5 @@ urlpatterns = [
     # path('data-log/', vehicleDataView.as_view(), name='vehicleDataView'),
     path('post-vehicle/', views.post_vehicle, name='post_vehicle'),
     path('get-vehicle/<str:vehicleNumber>/', views.get_vehicle, name='get_vehicle'),
-
 
 ]
